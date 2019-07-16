e1a48085f5e1f9e38ded98b05d05fdd316b41f53
wang.weiyi <wang.weiyi@chinaredstar.com>
2019-07-16 19:20:55 +0800

    dubbo

--- service/src/main/java/com/wwy/ServiceApplication.java
+++ service/src/main/java/com/wwy/ServiceApplication.java
@@ -2,14 +2,18 @@ package com.wwy;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 @EnableDubboConfig
 public class ServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceApplication.class, args);
+        new SpringApplicationBuilder(ServiceApplication.class)
+                .web(WebApplicationType.NONE) // NONE为非web工程，一般用于专门提供接口的jar
+                .run(args);
     }
 
 }
